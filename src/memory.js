eedc5c157d5e43935715d1eb8058dd2dc5c58027
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-07-13 18:18:20 +0200

    Update memory.js

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(cards) {
+    if (!cards) {
+      return undefined;
+    }
+
+    const shuffledCards = [...cards]; // Create a copy of the cards array
+
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledCards[i], shuffledCards[j]] = [
+        shuffledCards[j],
+        shuffledCards[i],
+      ];
+    }
+
+    return shuffledCards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    let guessed = card1 === card2;
+    if (guessed) {
+      this.pairsGuessed++;
+    } else {
+      return false;
+    }
+    return guessed;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2;
   }
 }
